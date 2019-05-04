e509cedc5b85f4e0a7817aad694c24a04a9078d2
unknown <dhanashri.galgalikar11@gmail.com>
2019-05-05 00:07:05 +0530

    Commit

---
+++ src/main/java/com/example/MakeMyPlan3/MakeMyPlan3Application.java
@@ -0,0 +1,13 @@
+package com.example.MakeMyPlan3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMyPlan3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeMyPlan3Application.class, args);
+	}
+
+}
